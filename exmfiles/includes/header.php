b5bd1d74299783f692488997cc8683d8ab08962d
lilith <263341466+spider-lilith@users.noreply.github.com>
2026-04-13 11:57:56 +0100

    Add header.php with basic HTML structure

---
+++ exmfiles/includes/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="includes/css/styles.css">
+    <link rel="stylesheet" href="includes/css/products.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="header-box">
+
+        <div class="header-img">
+            <img src='img/GLH-logo.png'>
+        </div>
+        
+        <div class="header-links">
+            <a href="index.php">Home</a>
+            <a href="products.php">Products</a>
+            <a href="#">About</a>
+            <a href="#">Farmers</a>
+        </div>
+
+        <div class="header-buttons">
+            <a href="sign-up.php">Login</a>
+            <a href="#">Cart</a>
+        </div>
+    
+    </div>
+
+</body>
+</html>
