34f04a651a2b52f21a33c5889d98e52d87b1a025
lilith <263341466+spider-lilith@users.noreply.github.com>
2026-04-14 14:09:23 +0100

    Update header.php to include Font Awesome and Google Fonts

--- exmfiles/includes/header.php
+++ exmfiles/includes/header.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="includes/css/styles.css">
-    <link rel="stylesheet" href="includes/css/products.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Chilanka&family=Rock+Salt&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -23,8 +26,12 @@
         </div>
 
         <div class="header-buttons">
-            <a href="sign-up.php">Login</a>
-            <a href="#">Cart</a>
+            <a href="sign-up.php" aria-label="Login">
+                <i class="fa-solid fa-circle-user"></i>
+            </a>
+            <a href="#" aria-label="Cart">
+                <i class="fa-solid fa-cart-shopping"></i>
+            </a>
         </div>
     
     </div>
