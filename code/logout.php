3f1614a52bea85f43669bc659accc8ad4263082d
lilith <263341466+spider-lilith@users.noreply.github.com>
2026-04-16 14:23:08 +0100

    Delete code directory

--- code/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$_SESSION = [];            // clear all session data
-session_destroy();         // destroy session
-session_unset();           // safety
-
-header('Location: /TLEVEL-exam/finished/login.php');
-exit;
